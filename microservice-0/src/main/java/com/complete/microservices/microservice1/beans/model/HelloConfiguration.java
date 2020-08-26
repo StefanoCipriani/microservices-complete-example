9aa1f65c5b2f9ec1540c964d3fe858d1b311f739
Stefano Cipriani <stefano.voce@gmail.com>
2020-08-26 14:51:56 +0200

    microservice-0 added (this will call all others)

---
+++ microservice-0/src/main/java/com/complete/microservices/microservice1/beans/model/HelloConfiguration.java
@@ -0,0 +1,27 @@
+package com.complete.microservices.microservice1.beans.model;
+
+public class HelloConfiguration {
+
+	private String message;
+	private String description;
+	
+	public HelloConfiguration(String message, String description) {
+		this.message=message;
+		this.description=description;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
