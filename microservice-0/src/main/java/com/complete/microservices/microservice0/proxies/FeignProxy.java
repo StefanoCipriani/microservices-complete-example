2a0074b112ab0f6bc02fcb74766c5b03097758d1
Stefano Cipriani <stefano.voce@gmail.com>
2020-08-27 09:35:26 +0200

    minore fix

--- microservice-0/src/main/java/com/complete/microservices/microservice0/proxies/FeignProxy.java
+++ microservice-0/src/main/java/com/complete/microservices/microservice0/proxies/FeignProxy.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.complete.microservices.microservice0.beans.model.HelloConfiguration;
 
-//@FeignClient(name ="microservice-1", url="localhost:9001" ) //with Ribbon no needs to specify the server:port couple
+//@FeignClient(name ="microservice-1", url="localhost:9001" ) //configuration without ribbon
 @RibbonClient(name ="microservice-1")
 @FeignClient(name ="microservice-1")
 public interface FeignProxy {
 
-	@GetMapping("hello")
+	@GetMapping("hello")//The name of rest of destination microservice
 	public ResponseEntity<HelloConfiguration> callMicroservice1Feign();
 }
